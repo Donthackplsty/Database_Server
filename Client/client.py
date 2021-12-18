2b18a1bb6ed2620554b3d3063fb0fcd99f083799
Donthackplsty <therealhussainali@gmail.com>
2021-12-18 21:42:30 +0500

    Renamed default.json to passwords.json - Starting to make client

---
+++ Client/client.py
@@ -0,0 +1,11 @@
+import os 
+
+def Main():
+    print("- - - - - - - - | Hello User! |- - - - - - - -")
+    print("=> Please Select From The Following Options:")
+    print("=> Check An Existing Password")
+    print("=> Add A New Password")
+
+if __name__ == "__main__":
+    Main()
+    
